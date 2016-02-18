360fe3726827c85f5591aefe52ce18bda0fbc0db
Максим Тюгаев <tiugaev@etton.ru>
2016-02-18 14:48:01 +0300

    alpha version

--- Chat.php
+++ Chat.php
@@ -1,21 +1,35 @@
-<?php
+<?php namespace tugmaks\Jivosite;
 
-namespace tugmaks\Jivosite;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\web\View;
 
 /**
  * Jivosite chat widget
  */
 class Chat extends \yii\base\Widget
 {
+
     public $widgetId;
+
     public function init()
     {
+        $this->widgetId = (null == $this->widgetId) ? array_key_exists('jivosite.widgetId', Yii::$app->params) ? Yii::$app->params['jivosite.widgetId'] :
+                null :
+            $this->widgetId;
+
+        if ($this->widgetId == null) {
+            throw new InvalidConfigException('No Jivosite widgetId specified');
+        }
         parent::init();
-        
     }
-    
+
     public function run()
     {
-        echo $this->widgetId;
+        $js = <<< JS
+(function(){ var widget_id = '{$this->widgetId}';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();
+JS;
+        $this->view->registerJs($js, View::POS_END);
     }
 }
