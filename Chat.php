b7db91dbb6b49911119bb1be8f32840aa918fcf3
ssv <ssv@medincom.info>
2018-12-20 11:09:33 +0500

    js update

--- Chat.php
+++ Chat.php
@@ -1,4 +1,6 @@
-<?php namespace tugmaks\Jivosite;
+<?php
+
+namespace tugmaks\Jivosite;
 
 use Yii;
 use yii\base\InvalidConfigException;
@@ -28,7 +30,7 @@ class Chat extends \yii\base\Widget
     {
         $js = <<< JS
 (function(){ var widget_id = '{$this->widgetId}';
-var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();
+var d=document;var w=window;function l(){var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 JS;
         $this->view->registerJs($js, View::POS_END);
     }
